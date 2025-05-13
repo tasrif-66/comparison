c51658fcc08c3cb163f676e5ea5554e37e99e723
tasrif-66 <mtasrif20@gmail.com>
2025-05-14 00:43:27 +0600

    comparision practice

---
+++ likeness.js
@@ -0,0 +1,23 @@
+
+// comparesion practice from book
+
+console.log(7 > 5);
+console.log(45 < 50);
+console.log(75 == 75);
+console.log(95 <= 100);
+
+console.log(7 != 10);
+console.log(20 <= 15);
+
+
+console.log(97 < 45);
+
+
+
+
+
+
+
+
+
+
