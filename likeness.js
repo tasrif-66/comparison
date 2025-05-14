f539a1d733ec0fc54f1fecab329841b689eea30e
tasrif-66 <mtasrif20@gmail.com>
2025-05-14 14:55:06 +0600

    practice

--- likeness.js
+++ likeness.js
@@ -1,16 +1,55 @@
 
 // comparesion practice from book
 
-console.log(7 > 5);
-console.log(45 < 50);
-console.log(75 == 75);
-console.log(95 <= 100);
+// console.log(7 > 5);
+// console.log(45 < 50);
+// console.log(75 == 75);
+// console.log(95 <= 100);
 
-console.log(7 != 10);
-console.log(20 <= 15);
+// console.log(7 != 10);
+// console.log(20 <= 15);
 
 
-console.log(97 < 45);
+// console.log(97 < 45);
+
+
+const brotherAge = 10;
+const myAge = 15;
+console.log(brotherAge <= myAge);
+
+console.log(33 <= 45);
+
+const jambura = 50;
+const kodbel = 30;
+console.log(jambura == kodbel);
+
+const myREad = 10;
+const myFriendRead = 10;
+console.log(myREad == myFriendRead);
+
+const iHave = 20;
+const friendHave = 15;
+console.log(friendHave > iHave);
+
+const fcb = 2;
+const barsa = 3;
+console.log(fcb < barsa);
+
+const myName = "Rahim";
+const friendName = "Karim";
+console.log(myName == friendName);
+
+const myreding = true;
+const friendreading = false;
+console.log(myreding != friendreading);
+
+
+
+
+
+
+
+
 
 
 
